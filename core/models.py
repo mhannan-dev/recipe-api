cba59cd122ef82c05e19d810ef09f1a0efa82ccd
Muhammad Hannan <mdhannan.info@gmail.com>
2024-12-09 23:10:02 +0600

    Added custom user model

---
+++ core/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager where email is the unique identifier
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with the given email and password.
+        """
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model where email is the unique identifier for authentication
+    instead of usernames.
+    """
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  
+
+    def __str__(self):
+        return self.email
--- rc_final_prj/wsgi.py
+++ rc_final_prj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for recipe project.
+WSGI config for rc_final_prj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rc_final_prj.settings')
 
 application = get_wsgi_application()
